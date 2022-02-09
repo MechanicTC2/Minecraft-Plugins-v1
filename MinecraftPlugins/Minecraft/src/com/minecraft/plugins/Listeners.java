2466a639701b9289e65231d0f627b7693266e32e
MechanicTC2 <76660425+MechanicTC2@users.noreply.github.com>
2022-02-09 11:19:05 -0800

    Update Listeners.java
    remove irrelevant methods

--- MinecraftPlugins/Minecraft/src/com/minecraft/plugins/Listeners.java
+++ MinecraftPlugins/Minecraft/src/com/minecraft/plugins/Listeners.java
@@ -49,77 +49,20 @@ public class Listeners extends OtherStuff implements Listener {
 	public void endWand(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
 		ItemMeta meta = player.getInventory().getItemInMainHand().getItemMeta();
-			if (meta.getDisplayName().equalsIgnoreCase(ChatColor.DARK_PURPLE + "End Wand")) {
-				if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {	
-					BlockFace face = BlockFace.WEST;
-					Location front2 = new Location(Bukkit.getWorld("world"), player.getTargetBlockExact(50).getX(), player.getTargetBlockExact(50).getY() + 1, player.getTargetBlockExact(50).getZ());
-					Location front3 = new Location(Bukkit.getWorld("world"), player.getTargetBlockExact(50).getX() + 4, player.getTargetBlockExact(50).getY() + 1, player.getTargetBlockExact(50).getZ() + 4);
-					Portal end_portal = new Portal();
-					end_portal.EndPortal(front2, front3, face);
-				}
-				if (event.getAction().equals(Action.LEFT_CLICK_AIR) || event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {	
-					BlockFace face = BlockFace.NORTH;
-					Location front2 = player.getTargetBlockExact(50).getLocation();
-					Location front3 = new Location(Bukkit.getWorld("world"), player.getTargetBlockExact(50).getX() + 4, player.getTargetBlockExact(50).getY(), player.getTargetBlockExact(50).getZ() + 4);
-					Portal end_portal = new Portal();
-					end_portal.EndPortal(front2, front3, face);
-				}
+		if (meta.getDisplayName().equalsIgnoreCase(ChatColor.DARK_PURPLE + "End Wand")) {
+			if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {	
+				BlockFace face = BlockFace.WEST;					Location front2 = new Location(Bukkit.getWorld("world"), player.getTargetBlockExact(50).getX(), player.getTargetBlockExact(50).getY() + 1, player.getTargetBlockExact(50).getZ());
+				Location front3 = new Location(Bukkit.getWorld("world"), player.getTargetBlockExact(50).getX() + 4, player.getTargetBlockExact(50).getY() + 1, player.getTargetBlockExact(50).getZ() + 4);
+				Portal end_portal = new Portal();
+				end_portal.EndPortal(front2, front3, face);
 			}
-		}
-	@EventHandler
-	public void Glock17(PlayerInteractEvent event) {
-		/*Player player = event.getPlayer();
-		ItemMeta meta = player.getInventory().getItemInMainHand().getItemMeta();
-		if (meta.getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "Glock 17") || meta.getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "Glock 17")) {
 			if (event.getAction().equals(Action.LEFT_CLICK_AIR) || event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
-				if (ammo >= 0) {
-					player.spawnParticle(Particle.ELECTRIC_SPARK, player.getTargetBlockExact(100).getLocation(), 20);
-					ammo --;
-					RayTraceResult result = player.getWorld().rayTraceEntities(player.getEyeLocation(), player.getEyeLocation().getDirection(), 100);
-					player.sendMessage(result.toString());
-					Entity entity = result.getHitEntity();
-					((Damageable) entity).setHealth(((Damageable) entity).getHealth() - 2);
-				}
-				else {
-					player.sendMessage("No more ammo");
-				}
-				
-			}
-		}*/
-		
-	}
-	@EventHandler
-	public void RPG(PlayerInteractEvent event) {
-		Player player = event.getPlayer();
-		ItemMeta meta = player.getInventory().getItemInMainHand().getItemMeta();
-			if (meta.getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "RPG")) {
-				if (event.getAction().equals(Action.LEFT_CLICK_AIR) || event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {	
-					Arrow arrow = player.launchProjectile(Arrow.class);
-					arrow.setVisualFire(true);
-					arrow.setVelocity(player.getLocation().getDirection().multiply(2));
-					
-				}
-			}
-	}
-	@EventHandler
-	public void RPGExplosion(ProjectileHitEvent event) {
-		if (event.getEntity() instanceof Arrow) {
-			if (event.getEntity().getShooter() instanceof Player) {
-				Player player = (Player) event.getEntity().getShooter();
-				ItemMeta meta = player.getInventory().getItemInMainHand().getItemMeta();
-				if (meta.getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "RPG")) {
-					for (Entity entity : event.getEntity().getNearbyEntities(3, 3, 3)) {
-						((Damageable) entity).setHealth(0);
-					}
-					for (Entity entity : event.getEntity().getNearbyEntities(5, 5, 5)) {
-						((Damageable) entity).setHealth(((Damageable) entity).getHealth() - 10);
-					}
-				}
+				BlockFace face = BlockFace.NORTH;
+				Location front2 = player.getTargetBlockExact(50).getLocation();
+				Location front3 = new Location(Bukkit.getWorld("world"), player.getTargetBlockExact(50).getX() + 4, player.getTargetBlockExact(50).getY(), player.getTargetBlockExact(50).getZ() + 4);
+				Portal end_portal = new Portal();
+				end_portal.EndPortal(front2, front3, face);
 			}
 		}
-	}
-	@EventHandler	
-	public void disableWeatherChange(WeatherChangeEvent event) {
-		event.setCancelled(true);
 	}
 }
