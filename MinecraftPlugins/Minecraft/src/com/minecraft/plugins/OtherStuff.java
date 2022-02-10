e4036e96bfe1533fd552ed22c86fb89a666567b6
MechanicTC2 <76660425+MechanicTC2@users.noreply.github.com>
2022-02-09 16:05:16 -0800

    Update OtherStuff.java

--- MinecraftPlugins/Minecraft/src/com/minecraft/plugins/OtherStuff.java
+++ MinecraftPlugins/Minecraft/src/com/minecraft/plugins/OtherStuff.java
@@ -20,54 +20,6 @@ import org.bukkit.inventory.meta.ItemMeta;
 import com.minecraft.plugins.Cuboid.CuboidDirection;
 
 public class OtherStuff {
-	int interval;
-	int ammo = 17;
-	public void createHole() {
-		Location first = new Location(Bukkit.getWorld("world"), -700, 62, -365);
-		Location second = new Location(Bukkit.getWorld("world"), -690, 52, -355 );
-		Cuboid hole = new Cuboid(first, second);
-		Cuboid barrier = hole.outset(CuboidDirection.Both, 1);
-		
-		for (Block block : barrier) {
-			block.setType(Material.BARRIER);
-		}
-		for (Block block : hole) {
-			block.setType(Material.AIR);
-		}
-	}
-	public void resetPyramid() {
-		Location first = new Location(Bukkit.getWorld("world"), -700, 62, -365);
-		Location second = new Location(Bukkit.getWorld("world"), -690, 32, -355 );
-		Location top = new Location(Bukkit.getWorld("world"), -690, 62, -355 );
-		Cuboid hole = new Cuboid(first, second);
-		Cuboid barrier = hole.outset(CuboidDirection.Both, 1);
-		Cuboid ceiling = new Cuboid (first, top);
-		
-		for (Block block : barrier) {
-			block.setType(Material.AIR);
-		}
-		for (Block block : hole) {
-			block.setType(Material.AIR);
-		}
-		for (Block block : ceiling) {
-			block.setType(Material.WATER);
-		}
-	}
-	public void createPyramid() {
-		Location bottom1 = new Location(Bukkit.getWorld("world"), -700, 52, -365);
-		Location bottom2 = new Location(Bukkit.getWorld("world"), -690, 52, -355 );
-		Location beacon_top = new Location (Bukkit.getWorld("world"), -695, 57, -360);
-		Cuboid bottom = new Cuboid(bottom1, bottom2);
-		Cuboid beacon = new Cuboid(beacon_top, beacon_top);
-		for (interval = 0; interval < 5; interval++) {
-			for (Block block : bottom.shift(CuboidDirection.Up, interval).inset(CuboidDirection.Horizontal, interval)) {
-				block.setType(Material.NETHERITE_BLOCK);
-			}
-		}
-		for (Block block : beacon) {
-			block.setType(Material.BEACON);
-		}
-	}
 	public ItemStack netherWand() {
 		ItemStack item = new ItemStack(Material.BLAZE_ROD);
 		ItemMeta meta = item.getItemMeta();
